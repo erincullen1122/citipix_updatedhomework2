ba13ec345517f2676c34c44118488c072fc66363
erincullen1122 <anniehall16@hotmail.com>
2016-04-11 12:31:09 -0700

    Added files via upload

---
+++ js/text.js
@@ -0,0 +1,42 @@
+  $(document).ready(function() {
+      console.log( "ready!" );
+
+
+    $("#submitbtn").click(switchBackground);
+  
+    function switchBackground(event) {
+
+      event.preventDefault();       
+      var city = $("#cityname").val();
+      city=city.toLowerCase();
+
+      if (city == "new york" || 
+          city == "nyc" || 
+          city == "new york city") {
+        $("body").attr('class', 'nyc'); /*attr adds and removes for you, so you can use for making images change on click*/
+      }
+
+      else if (city == "san francisco" || 
+          city == "sf" || 
+          city == "san fran") {
+        $("body").attr("class","sf"); 
+      }
+
+         else if (city == "los angeles" || 
+          city == "la" || 
+          city == "las angeles") {
+        $("body").attr("class","la"); 
+      }
+
+         else if (city == "austin" || 
+          city == "austin" || 
+          city == "austin") {
+        $("body").attr("class","austin"); 
+      }
+
+         else  
+        $("body").attr("class","sydney"); 
+      }
+
+
+  });
